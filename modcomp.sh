8099329d4fa9166b02ac89f819d809ec68e6ad57
mattroz <matveyrozanov@gmail.com>
2016-11-08 13:17:21 +0300

    Exec script with path to module you want to build

--- modcomp.sh
+++ modcomp.sh
@@ -1,6 +1,10 @@
 #/bin/bash
 
 #	get current module name from the absolute path
-MODULE_NAME="${PWD##*/}"
+MODULE_PATH=$1  #"${PWD##*/}"
 #echo $MODULE_NAME
 
+cd $MODULE_PATH
+/usr/local/qt5pi/bin/qmake .
+make -j$(nproc --all)
+sudo make install
